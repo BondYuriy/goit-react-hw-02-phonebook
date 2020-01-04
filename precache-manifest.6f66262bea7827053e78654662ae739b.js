4d11e3985031dc8f8ba52e0e2526ba35ba9025ad
Yuriy Bondar <bondar.europe@gmail.com>
2020-01-04 13:09:50 +0200

    Updates

--- precache-manifest.6f66262bea7827053e78654662ae739b.js
+++ precache-manifest.6f66262bea7827053e78654662ae739b.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "06211aa2a6759674ff8d8b3ff67666e0",
+    "revision": "a79ffdd45631076680f12b28c9bf0db8",
     "url": "/goit-react-hw-02-phonebook/index.html"
   },
   {
-    "revision": "86bb200dd9a5018ffd76",
+    "revision": "a8b001d5fde8298f564e",
     "url": "/goit-react-hw-02-phonebook/static/css/main.0564f79e.chunk.css"
   },
   {
-    "revision": "bd87e112830981d9bede",
-    "url": "/goit-react-hw-02-phonebook/static/js/2.1e4ee55d.chunk.js"
+    "revision": "5da87f3755299ff352e7",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.b3803d37.chunk.js"
   },
   {
     "revision": "d705cb622423d72c5defbf368ca70dcc",
-    "url": "/goit-react-hw-02-phonebook/static/js/2.1e4ee55d.chunk.js.LICENSE"
+    "url": "/goit-react-hw-02-phonebook/static/js/2.b3803d37.chunk.js.LICENSE"
   },
   {
-    "revision": "86bb200dd9a5018ffd76",
-    "url": "/goit-react-hw-02-phonebook/static/js/main.20407fd9.chunk.js"
+    "revision": "a8b001d5fde8298f564e",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.51416d5d.chunk.js"
   },
   {
     "revision": "d8b79e73e21392c082a4",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react-hw-02-phonebook/precache-manifest.b07e65cf8447bc156997972310f4f01e.js"
+  "/goit-react-hw-02-phonebook/precache-manifest.6f66262bea7827053e78654662ae739b.js"
 );
 
 self.addEventListener('message', (event) => {
